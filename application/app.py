3fed53973f2b065ccdc83aac0000daeb9acd30d5
Adland Lee <adland@4mfd.com>
2015-12-16 18:06:36 -0800

    refactor application files
    Move project.py to application/app.py to keep from cluttering
    directories.

--- application/app.py
+++ application/app.py
@@ -7,6 +7,10 @@ app.secret_key = 'super_secret_key'
 def showHome():
     return "Home"
 
+@app.route('/test')
+def showTest():
+    return "Test"
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
