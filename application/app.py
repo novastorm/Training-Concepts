1e38b4d8c58d6b449a759328ada2ad88d23cd714
Adland Lee <adland@4mfd.com>
2015-12-20 17:34:50 -0800

    add skill routes

--- application/app.py
+++ application/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 # from routes.v0.course import app as course_bp
 from routes.v0.course import api as api_v0_course_bp
 from routes.v0.course.skill import api as api_v0_course_skill_bp
+from routes.v0.skill import api as api_v0_skill_bp
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
@@ -10,6 +11,7 @@ app.secret_key = 'super_secret_key'
 # app.register_blueprint(course_bp)
 app.register_blueprint(api_v0_course_bp, url_prefix='/api/v0')
 app.register_blueprint(api_v0_course_skill_bp, url_prefix='/api/v0')
+app.register_blueprint(api_v0_skill_bp, url_prefix='/api/v0')
 
 @app.route('/')
 def showHome():
---
+++ application/routes/v0/skill.py
@@ -0,0 +1,68 @@
+import flask
+
+from flask import Blueprint
+from flask import abort
+from flask import jsonify
+from flask import request
+
+app = Blueprint('skill', __name__)
+api = Blueprint('skill_api', __name__)
+
+label = "Course-Skill"
+
+def _getIndex():
+    """Retrieve %s index data
+
+    return index data"""
+    return "%s Index" % label
+
+def _showRecord(req):
+    """Show Record"""
+    return "%s _showRecord" % label
+
+def _storeRecord(req):
+    """Store Record"""
+    return "%s _storeRecord" % label
+
+def _updateRecord(req):
+    """Update Record"""
+    return "%s _updateRecord" % label
+
+def _destroyRecord(req):
+    """Destroy Record"""
+    return "%s _destroyRecord" % label
+
+
+###############################################################################
+#
+# API routes
+#
+
+@api.route('/skills')
+@api.route('/skills.json')
+def api_index():
+    return jsonify(skill_list=['index.json'], req=request.form)
+
+@api.route('/skills/<int:skill_id>')
+@api.route('/skills/<int:skill_id>.json')
+def api_show(skill_id):
+    setattr(request, 'skill_id', skill_id)    
+    record = _showRecord(request)
+    return jsonify(skill=record)
+
+@api.route('/skills', methods=['POST'])
+def api_store():
+    record = _storeRecord(request)
+    return jsonify(skill=record)
+
+@api.route('/skills/<int:skill_id>', methods=['PUT'])
+def api_update(skill_id):
+    setattr(request, 'skill_id', skill_id)
+    record = _updateRecord(request)
+    return jsonify(skill=record)
+
+@api.route('/skills/<int:skill_id>', methods=['DELETE'])
+def api_destroy(skill_id):
+    setattr(request, 'skill_id', skill_id)
+    record = _destroyRecord(request)
+    return jsonify(skill=record)
