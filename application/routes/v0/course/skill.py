ee8b2a8aecc61ac8218448514f30e13444f525c0
Adland Lee <adland@4mfd.com>
2015-12-25 17:21:55 -0800

    refactor course skill
    clean up responses and test case

--- application/routes/v0/course/skill.py
+++ application/routes/v0/course/skill.py
@@ -14,15 +14,15 @@ def _getIndex(req):
     """Retrieve %s index data
 
     return index data""" % label
-    return "%s Index" % label
+    return "%s %s Index" % (label, req.course_id)
 
 def _storeRecord(req):
     """Store Record"""
-    return "%s _storeRecord" % label
+    return "%s %s _storeRecord" % (label, req.course_id)
 
 def _destroyRecord(req):
     """Destroy Record"""
-    return "%s _destroyRecord" % label
+    return "%s %s %s _destroyRecord" % (label, req.course_id, req.skill_id)
 
 
 ###############################################################################
@@ -35,17 +35,17 @@ def _destroyRecord(req):
 def api_index(course_id):
     setattr(request, 'course_id', course_id)  
     records = _getIndex(request)  
-    return jsonify(skill_list=[records])
+    return records
 
 @api.route('/courses/<int:course_id>/skills', methods=['POST'])
 def api_store(course_id):
     setattr(request, 'course_id', course_id)    
     record = _storeRecord(request)
-    return jsonify(skill=record)
+    return record
 
 @api.route('/courses/<int:course_id>/skills/<int:skill_id>', methods=['DELETE'])
 def api_destroy(course_id, skill_id):
     setattr(request, 'course_id', course_id)
     setattr(request, 'skill_id', skill_id)
     record = _destroyRecord(request)
-    return jsonify(skill=record)
+    return record
--- tests/tests/course/skill.py
+++ tests/tests/course/skill.py
@@ -4,6 +4,10 @@ import unittest
 from app import app
 
 label = "Course-Skill"
+res_label = 'courses'
+res_id = '1'
+subres_label = 'skills'
+subres_id = '2'
 
 class apiCourseSkillTestCase(unittest.TestCase):
     def setUp(self):
@@ -13,16 +17,16 @@ class apiCourseSkillTestCase(unittest.TestCase):
         pass
 
     def test_index(self):
-        res = self.app.get('/api/v0/courses/1/skills')
-        data = json.loads(res.data)
-        assert data['skill_list'][0] == "%s Index" % label
+        res = self.app.get('/api/v0/%s/%s/%s' % (res_label, res_id, subres_label))
+        data = res.data
+        assert data == "%s %s Index" % (label, res_id)
 
     def test_store(self):
-        res = self.app.post('/api/v0/courses/1/skills')
-        data = json.loads(res.data)
-        assert data['skill'] == "%s _storeRecord" % label
+        res = self.app.post('/api/v0/%s/%s/%s' % (res_label, res_id, subres_label))
+        data = res.data
+        assert data == "%s %s _storeRecord" % (label, res_id)
 
     def test_destroy(self):
-        res = self.app.delete('/api/v0/courses/1/skills/1')
-        data = json.loads(res.data)
-        assert data['skill'] == "%s _destroyRecord" % label
+        res = self.app.delete('/api/v0/%s/%s/%s/%s' % (res_label, res_id, subres_label, subres_id))
+        data = res.data
+        assert data == "%s %s %s _destroyRecord" % (label, res_id, subres_id)
